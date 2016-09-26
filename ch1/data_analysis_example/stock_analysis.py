4cd946f28a0de101cfd74986518f2a217db67c60
behappycc <behappyccc@gmail.com>
2016-09-26 23:52:31 +0800

    add data_analysis_example

---
+++ ch1/data_analysis_example/stock_analysis.py
@@ -0,0 +1,16 @@
+# pandas module
+
+import pandas as pd
+
+def get_mean_tv_cp(symbol):
+    df = pd.read_csv("data/{}.csv".format(symbol))
+    return df['Trade_Volume'].mean(), df['Closing_Price'].mean()
+
+def main():
+    for symbol in ['2330', '2454', '2498']:
+        print (symbol + " Mean Closing_Price" + "Mean Trade Volume")
+        mean_trade_volume, mean_closing_price = get_mean_tv_cp(symbol)
+        print (mean_trade_volume, mean_closing_price)
+
+if __name__ == "__main__":
+    main()
