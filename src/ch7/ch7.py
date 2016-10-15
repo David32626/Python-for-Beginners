55467b80df75eb7af34f9bba19baef5285c98906
David32626 <bluesky32210@gmail.com>
2016-10-15 16:33:25 +0800

    CH7範例修改

--- src/ch7/ch7.py
+++ src/ch7/ch7.py
@@ -1,12 +1,4 @@
 '''
-Import module example
-'''
-import math
-print (math.factorial(3))
-print (math.fsum([1, 1, 1]))
-print (math.cos(math.pi))
-
-'''
 Write your module
 '''
 import calculator
@@ -16,85 +8,36 @@ print (calculator.multiplied(3, 4))
 print (calculator.divided(10, 5))
 
 '''
-Import third- party packages
-pip install numpy
-'''
-import numpy
-a = numpy.arange(15).reshape(3, 5)
-print (a)
-
-'''
-Import third- party packages
-change import module name
-'''
-import numpy as np
-a = np.arange(15).reshape(3, 5)
-print (a)
-
-'''
-from xxx import yyy
-'''
-from orderedlist import OrderedList
-mylist = OrderedList()
-mylist.add(31)
-mylist.add(77)
-mylist.add(17)
-mylist.add(93)
-mylist.add(26)
-mylist.add(54)
-
-print (mylist.size())
-print (mylist.search(93))
-print (mylist.search(100))
-
-'''
-time module example
-datetime(), date(), time(), today()
-import timedelta
-
-'''
-import time
-print (time.localtime())
-print (time.ctime())
-
-time.sleep(2)
-
-'''
 datetime module example
 '''
 from datetime import datetime
-print (datetime.today())
-print (datetime.now())
+d1 = datetime.now()
+print(d1)
+print(d1.date())
 
-from datetime import timedelta
-print (datetime.now()  + timedelta(hours = 1))
+d2 = datetime(2016,10,17)
+print(d2)
+
+delta = d2 - d1
+print(delta)
+print(delta.days)
+print(delta.seconds)
+
+s1 = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(s1,type(s1))
+
+d1 = datetime.strptime("2016-10-15 15:26:13","%Y-%m-%d %H:%M:%S")
+print(d1,type(d1))
+
 
 '''
 os module example
 '''
 import os
-now = os.getcwd()
 print (os.getcwd())
 print (os.listdir())
-workspace = 'C:\\Users\\hubert\\Desktop\\github\\Python-for-Beginners\\src\\ch7\\workspace'
-os.chdir(workspace)
-print (os.getcwd())
-os.chdir(now)
+os.mkdir("test")
+print (os.listdir())
+os.rmdir("test")
+print (os.listdir())
 
-for file in os.walk(now):
-    print (file)
-
-#os.mkdir('test1')
-#os.rename('test1', 'test2')
-#os.remove('test2/doc.txt')
-#os.rmdir('test2')
-
-'''
-os.path module example
-'''
-file = 'C:\\Users\\hubert\\Desktop\\github\\Python-for-Beginners\\src\\ch7'
-print (os.path.abspath(file))
-print (os.path.dirname(file))
-print (os.path.basename(file))
-
-print (os.path.exists(file))
