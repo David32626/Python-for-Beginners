682457d3c5b3da4b1fe03ee4714c333d896abd1c
David32626 <bluesky32210@gmail.com>
2016-11-01 00:40:05 +0800

    Update CH12

--- src/ch12/WeatherAnalyzer.py
+++ src/ch12/WeatherAnalyzer.py
@@ -1,6 +1,5 @@
 # import standard module
 from datetime import datetime, timedelta
-import xml.etree.ElementTree as ET
 
 # numpy module
 import numpy as np
@@ -8,64 +7,44 @@ import numpy as np
 # matplotlib module
 import matplotlib.pyplot as plt
 
-# weather data
-data_path = 'data/C-B0024-001.xml'
+import sys
+import json
 
 class WeatherAnalyzer():
-    def __init__(self, data_path, location):
-        self.data_path = data_path
-        self.location = location
+    def __init__(self):
+        self.data = {}
+        self.location_rainfall_list = {}
 
-    def read_data(self):
-        tree = ET.parse(self.data_path)
-        root = tree.getroot()
-        return root
+    def load_json(self,input_file):
+        jsonFile = open(input_file, encoding='utf8')
+        self.data = json.load(jsonFile)
+        jsonFile.close()
 
-    def parse_data(self, root):
-        # Parse data information
-        for item in root.iter('cwbopendata'):
-            self.identifier = item[0].text
-            self.sender = item[1].text
-            self.sent = item[2].text
-            self. status = item[3].text
-            self.status = item[4].text
-            self.dataid = item[5].text
-            self.scope = item[6].text
-
-        # Parse locationName, stationId, obsTime, temperature, humidity, rainfall
-        daily_data = []
-        for location in root.iter('location'):       
-            if location[0].text == self.location:
-                for time in location.iter('time'):
-                    hours_data = {}
-                    hours_data['locationName'] = location[0].text
-                    hours_data['stationId'] = location[1].text
-
-                    hours_data['obsTime'] = self.parse_time(time[0].text)
-
-                    try:
-                        hours_data['temperature'] = float(time[2][1][0].text)
-                    except ValueError as e:
-                        hours_data['temperature'] = 0
-
-                    #print (time[3][0].text)
-                    try:
-                        hours_data['humidity'] = float(time[3][1][0].text)
-                    except ValueError as e:
-                        hours_data['humidity'] = 0
-
-                    #print (time[6][0].text)
-                    try:
-                        hours_data['rainfall'] = float(time[6][1][0].text)
-                    except ValueError as e:
-                        hours_data['rainfall'] = 0
-                
-                    daily_data.append(hours_data)
-
-        return daily_data
+    def analyze_data(self):
+        for location in self.data:
+            print(location)
+            self.location_rainfall_list[location] = []
+            monthly_rainfall = 0.0
+            current_month = 9
+            for hour in self.data[location]:
+                dt = self.parse_time(hour["time"])
+                if dt.month == current_month:
+                    if hour["rainfall"] == "T": # rainfall less than 0.1mm
+                        continue
+                    monthly_rainfall += float(hour["rainfall"])
+                else:
+                    self.location_rainfall_list[location].append(monthly_rainfall)
+                    current_month += 1
+                    if current_month > 12:
+                        current_month -= 12
+                    monthly_rainfall = 0
+                    if hour["rainfall"] == "T":
+                        continue
+                    monthly_rainfall += float(hour["rainfall"])
+            print(self.location_rainfall_list[location])
 
     def parse_time(self, time):
-        time_format = '%Y/%m/%d %H:%M'
+        time_format = '%Y-%m-%d %H:%M'
         try:
             dt = datetime.strptime(time, time_format)
             return dt
@@ -75,57 +54,35 @@ class WeatherAnalyzer():
             dt += timedelta(hours=1)
             return dt
 
-    def mean_temperature(self, daily_data, month):
-        temperature = []
-        for data in daily_data:
-            date = data['obsTime'].timetuple()
-            # date[0] -> year, date[1] -> month, date[2] -> day, date[3] -> hour
-            if date[1] == month:
-                temperature.append(data['temperature'])
-        total = 0
-        noise_num = 0
-        for temp in temperature:
-            if temp == 0:
-                noise_num += 1
-            else:
-                total += temp
-        #print (len(temperature))
-        #print (temperature)
-        mean_temperature = round(total / (len(temperature) - noise_num), 2)
-        print ('Mean temperature: {}'.format(mean_temperature))
-        return (month, mean_temperature)
-
-    def plot_temperature(self, data):
-        
-        month = [x for x in range(1, 13)]
-        n_groups = len(month)
-
-        means_temperatures = [0, 0, 0, 0, 0, 0, 0, 0, 26.64, 26.39, 0, 0]
-
+    def plot_rainfall(self):
+        n_groups = 12
         fig, ax = plt.subplots()
         index = np.arange(n_groups)
-        bar_width = 0.1
 
-        rects1 = plt.bar(index, means_temperatures, bar_width, color='b', label='Men')
+        bar_width = 0.35
 
-        plt.xlabel('Temperature')
-        plt.ylabel('Month')
-        plt.title('Weather')
-        plt.xticks(index + bar_width, month)
+        opacity = 0.4
+
+        plt.title("Rainfall per month")
+        plt.xlabel("Month")
+        plt.ylabel("Rainfall per month(mm)")
+        plt.xticks(index,("201509","10","11","12","201601","02","03","04","05","06","07","08"))
+        taipei_rect = plt.bar(left=index,height=tuple(self.location_rainfall_list["Taipei"]),width=bar_width\
+                            ,align="center",color="b",label="Taipei",alpha=opacity)
+        kaohsiung_rect = plt.bar(left=index+bar_width,height=tuple(self.location_rainfall_list["Kaohsiung"]),width=bar_width\
+                            ,align="center",color="r",label="Kaohsiung",alpha=opacity)
         plt.legend()
 
+        plt.tight_layout()
         plt.show()
 
 
 def main():
-    wa = WeatherAnalyzer(data_path, 'Tamsui,淡水')
-    root = wa.read_data()
-    daily_data = wa.parse_data(root)
-    month_temperature = [x for 0 in range(0, 12)]
-    wa.mean_temperature(daily_data, 9)
-    wa.mean_temperature(daily_data, 10)
-
-    wa.plot_temperature([26.64, 26.39])
+    input_file = sys.argv[1]
+    analyzer = WeatherAnalyzer()
+    analyzer.load_json(input_file)
+    analyzer.analyze_data()
+    analyzer.plot_rainfall()
 
 if __name__ == '__main__':
     main()
---
+++ src/ch12/xmlParser.py
@@ -0,0 +1,48 @@
+# import standard module
+import xml.etree.ElementTree as ET
+
+import sys
+import os
+import json
+
+class WeatherXMLParser():
+    def __init__(self, input_file):
+        self.input_file = input_file
+        self.targetList = ["TAIPEI,臺北","CHIAYI,嘉義","KAOHSIUNG,高雄"]
+
+    def read_data(self):
+        print("read data")
+        tree = ET.parse(self.input_file)
+        self.root = tree.getroot()
+
+    def parse_data(self):
+        # Parse data information
+        data = {}
+        dataset = self.root[7]
+        for location in dataset:
+            location_name = location[0]
+            if location_name.text in self.targetList:
+                print("location = ",location_name.text)
+                data[location_name.text] = []
+                for child in location[2]:
+                    if child.tag == "{urn:cwb:gov:tw:cwbcommon:0.1}time":
+                        tempDict = {}
+                        tempDict["time"] = child[0].text
+                        tempDict["rainfall"] = child[6][1][0].text
+                        tempDict["temperature"] = child[2][1][0].text
+                        data[location_name.text].append(tempDict)
+
+        with open("weather_data.json", "w", encoding='utf8') as outfile:
+            json.dump(data, outfile, indent=4, ensure_ascii=False)
+
+        return []
+
+def main():
+    input_file = sys.argv[1]
+    parser = WeatherXMLParser(input_file)
+    parser.read_data()
+    parser.parse_data()
+    
+
+if __name__ == '__main__':
+    main()
